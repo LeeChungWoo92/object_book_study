333219f7710b782583cb17b20ab16c9c89a91848
LeeChungWoo <skcjddn@gmail.com>
2022-01-16 15:29:03 +0900

    refactor: Bag의 존재를 내부로 캡슐화

--- object_exam/src/main/java/com/lcw/study/object_exam/Audience.java
+++ object_exam/src/main/java/com/lcw/study/object_exam/Audience.java
@@ -6,11 +6,23 @@ package com.lcw.study.object_exam;
 public class Audience {
     private Bag bag; //관람객은 소지품을 보관하기 위해 가방을 소지할 수 있다.
 
-    public Audience(Bag bag){
+    public Audience(Bag bag) {
         this.bag = bag;
     }
 
-    public Bag getBag(){
+  /*  public Bag getBag(){ //chapter1_2
         return bag;
+    }*/
+
+    public Long buy(Ticket ticket) {
+        if (bag.hasInviation()) {
+            bag.setTicket(ticket);
+            return 0L;
+        } else {
+            bag.setTicket(ticket);
+            bag.minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
+
     }
 }
