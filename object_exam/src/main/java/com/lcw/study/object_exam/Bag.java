1be7b481ba48b9544dd1ee68b22e5af131610e1c
LeeChungWoo <skcjddn@gmail.com>
2022-01-16 14:08:34 +0900

    add: Bag class

---
+++ object_exam/src/main/java/com/lcw/study/object_exam/Bag.java
@@ -0,0 +1,42 @@
+package com.lcw.study.object_exam;
+
+/**
+ * 관람객이 소지품을 보관할 Bag
+ **/
+public class Bag {
+    private Long amount; //현금
+    private Inviation inviation; //초대장
+    private Ticket ticket; //티켓
+
+    public Bag(long amount) {  //초대장 없이 현금만 보관
+        this(null, amount);
+    }
+
+    public Bag(Inviation inviation, long amount) { //초대장,현금 보관
+        this.inviation = inviation;
+        this.amount = amount;
+    }
+
+
+    public boolean hasInviation() { //초대장 보유 여부
+        return inviation != null;
+    }
+
+    public boolean hasTicket() { //티켓 소유 여부
+        return ticket != null;
+    }
+
+    public void setTicket(Ticket ticket) { //초대장을 티켓으로 교환
+        this.ticket = ticket;
+    }
+
+    public void minusAmount(Long amount) { //현금 증가
+        this.amount -= amount;
+    }
+
+    public void plusAmount(Long amount) { //현금 감소
+        this.amount += amount;
+    }
+
+
+}
